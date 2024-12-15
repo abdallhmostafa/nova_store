5bffa8c3bd10c9a095c1e36cc37723263343ab19
Abdallh Mostafa elRabiey <abdallhelrabiey255@gmail.com>
2024-12-15 23:55:39 +0200

    feat: initialize Firebase and add environment variable management

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nova_store/core/app/env.variables.dart';
 import 'package:nova_store/nove_store.dart';
 
-void main() {
+void main() async {
+  await setup();
   runApp(const NovaStore());
+}
+
+Future<void> setup() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.wait([
+    EnvVariable.instance.init(env: EnvTypeEnum.dev),
+    Firebase.initializeApp(),
+    SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+    ),
+  ]);
 }
