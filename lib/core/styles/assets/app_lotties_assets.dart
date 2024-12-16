1c7ccba4047f73cba02a77985a4a34842b228058
Abdallh Mostafa elRabiey <abdallhelrabiey255@gmail.com>
2024-12-16 22:09:23 +0200

    feat: implement connectivity handling and add themed UI components

---
+++ lib/core/styles/assets/app_lotties_assets.dart
@@ -0,0 +1,12 @@
+const String _basePath = 'assets/images/lotties';
+
+class AppLottiesAssets {
+  AppLottiesAssets._();
+
+  static const String empty_1 = '$_basePath/empty_1.lottie';
+  static const String empty_2 = '$_basePath/empty_2.lottie';
+  static const String empty_3 = '$_basePath/empty_3.lottie';
+  static const String empty_4 = '$_basePath/empty_4.lottie';
+  static const String underDevelopment_1 = '$_basePath/under_development_1.lottie';
+  static const String underDevelopment_2 = '$_basePath/under_development_2.lottie';
+}
