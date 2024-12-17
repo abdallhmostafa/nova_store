35ac8f05ce3de318468142d8ae3c4c2537e97249
Abdallh Mostafa elRabiey <abdallhelrabiey255@gmail.com>
2024-12-17 13:09:45 +0200

    feat: add font constants and update HomePage to use custom fonts

---
+++ lib/core/styles/fonts/font_wieght_helper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FontWieghtHelper {
+  FontWieghtHelper._();
+  static final FontWieghtHelper instance = FontWieghtHelper._();
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight bold = FontWeight.w700;
+}
